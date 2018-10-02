aee83bf716ebaebbb021a65446fae7b06c5806a7
Wojciech Tobis <Wojciech.Tobis@novelis.com>
2018-10-02 21:47:07 +0200

    Add tests for DateParserUtil

---
+++ dateRangeTests/BaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using dateRange.Utils;
+using dateRange.Utils.Interfaces;
+
+namespace dateRangeTests
+{
+    public class BaseTests
+    {
+        protected IContainer container;
+
+        public BaseTests()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DateParserUtil>().As<IDateParserUtil>();
+
+            container = builder.Build();
+        }
+    }
+}
---
+++ dateRangeTests/Utils/DateParserUtilTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dateRange.Utils;
+using System;
+using dateRange.Utils.Interfaces;
+using dateRangeTests;
+using Autofac;
+
+namespace dateRange.Utils.Tests
+{
+    [TestClass()]
+    public class DateParserUtilTests : BaseTests
+    {
+        [TestMethod()]
+        public void ParseDateTestForValidInput()
+        {
+            IDateParserUtil dateParser = container.Resolve<IDateParserUtil>();
+            string dateString = "01.01.2016";
+
+            DateTime result = dateParser.ParseDate(dateString).Value;
+            DateTime expected = DateTime.Parse(dateString);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ParseDateTestForInvalidInput()
+        {
+            IDateParserUtil dateParser = container.Resolve<IDateParserUtil>();
+            string dateString = "01.01.2016r";
+
+            DateTime? result = dateParser.ParseDate(dateString);
+            DateTime? expected = null;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
