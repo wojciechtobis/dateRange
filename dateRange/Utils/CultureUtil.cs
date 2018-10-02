6d4a3e304d1941a6cd308941782eb467004a43d9
Wojciech Tobis <Wojciech.Tobis@novelis.com>
2018-10-02 21:59:14 +0200

    Add empty CultureUtil

---
+++ dateRange/Utils/CultureUtil.cs
@@ -0,0 +1,18 @@
+﻿using dateRange.Utils.Interfaces;
+
+namespace dateRange.Utils
+{
+    public class CultureUtil : ICultureUtil
+    {
+        /// <summary>
+        /// Date separtor specific for user's culture
+        /// </summary>
+        public string DateSeparator { get; }
+
+        /// <summary>
+        /// Short date pattern specific for user's culture
+        /// </summary>
+        public string ShortDatePattern { get; }
+
+    }
+}
