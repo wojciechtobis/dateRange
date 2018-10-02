3297ea662dc672346b183660ed7d5728c5ed8340
Wojciech Tobis <Wojciech.Tobis@novelis.com>
2018-10-02 21:41:23 +0200

    Add empty DateParserUtil

---
+++ dateRange/Utils/DateParserUtil.cs
@@ -0,0 +1,29 @@
+﻿using dateRange.Logging.Interfaces;
+using dateRange.Utils.Interfaces;
+using dateRange.Validation.Interfaces;
+using dateRange.Validation.Validations;
+using System;
+
+namespace dateRange.Utils
+{
+    public class DateParserUtil : IDateParserUtil
+    {
+        public DateParserUtil()
+        {            
+        }
+
+        /// <summary>
+        /// Method which parse string to DateTime with validation
+        /// </summary>
+        /// <param name="dateString">
+        /// String with date for parsing
+        /// </param>
+        /// <returns>
+        /// If input can be parsed method returns DateTime, otherwise returns null
+        /// </returns>
+        public DateTime? ParseDate(string dateString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dateRange/Utils/Interfaces/IDateParserUtil.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace dateRange.Utils.Interfaces
+{
+    public interface IDateParserUtil
+    {
+        DateTime? ParseDate(string dateString);
+    }
+}
