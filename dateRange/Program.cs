7367894ca68441d9879d2ea7d0fb386207f787fa
Wojciech Tobis <Wojciech.Tobis@novelis.com>
2018-10-03 22:14:53 +0200

    Implement program logic

--- dateRange/Program.cs
+++ dateRange/Program.cs
@@ -1,6 +1,9 @@
 using Autofac;
 using dateRange.Autofac;
+using dateRange.Interfaces;
 using dateRange.Logging.Interfaces;
+using dateRange.Utils.Interfaces;
+using dateRange.Validation.Validations;
 using System;
 
 namespace dateRange
@@ -18,15 +21,35 @@ namespace dateRange
 
                 try
                 {
+                    ArgsCounterValidation argsCounterValidator = new ArgsCounterValidation(args);
 
+                    if (argsCounterValidator.IsValid)
+                    {
+                        IDateParserUtil dateParserUtil = scope.Resolve<IDateParserUtil>();
+
+                        DateTime? startDate = dateParserUtil.ParseDate(args[0]);
+                        DateTime? endDate = dateParserUtil.ParseDate(args[1]);
+
+                        if (startDate != null && endDate != null)
+                        {
+                            IDateRangeParser dateRangeParser = scope.Resolve<IDateRangeParser>();
+                            string range = dateRangeParser.CalculateRange(startDate.Value, endDate.Value);
+                            if (range != null)
+                            {
+                                logger.Info(range);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        logger.Error(argsCounterValidator.Message);
+                    }
                 }
                 catch(Exception e)
                 {
                     logger.Fatal(string.Format("Unknown exception: {0}", e.ToString()));
                 }
             }
-
-            Console.ReadKey();
         }
     }
 }
